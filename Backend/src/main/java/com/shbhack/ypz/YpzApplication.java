83e0036d9c62ab1153d8787e402b463d951ea750
Donggyeom <eastcrtsy@gmail.com>
2023-09-02 22:41:50 +0900

    chore: 프로젝트 초기 생성

---
+++ Backend/src/main/java/com/shbhack/ypz/YpzApplication.java
@@ -0,0 +1,13 @@
+package com.shbhack.ypz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YpzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YpzApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/shbhack/ypz/YpzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shbhack.ypz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YpzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
