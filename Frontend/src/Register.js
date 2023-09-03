ba4c56695f034f86c66183fe1474637f794de48c
pplemover <paulimbest@naver.com>
2023-09-03 20:34:03 +0900

    feat: 사용자 이름과 비밀번호 유효성 검사 관련 코드 추가

--- Frontend/src/Register.js
+++ Frontend/src/Register.js
@@ -1,1 +1,8 @@
 import { useRef, useState, useEffect } from "react";
+import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from './api/axios';
+
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/; // 사용자 이름의 유효성을 검사하는 정규식
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/; // 비밀번호의 유효성을 검사하는 정규식
+const REGISTER_URL = '/register'; // 등록 API의 URL
