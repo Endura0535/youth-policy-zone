f32edec3b27e9f7c2b2c85509175d9ffbe95fa79
pplemover <paulimbest@naver.com>
2023-09-10 23:40:47 +0900

    feat: AuthPage 로그인/회원가입 토글스위치 구현

--- Frontend/src/pages/WelcomePage.js
+++ Frontend/src/pages/WelcomePage.js
@@ -5,7 +5,12 @@ import shinhanlogo from '../assets/images/shinhanlogo.png';
 function WelcomePage() {
   return (
     <div>
-      <img src={shinhanlogo} alt="내 이미지" class="welcomelogo"/>
+      <div class='brand yflex'>
+        <img src={shinhanlogo} alt="내 이미지" class="welcomelogo"/>
+        <div class="brand-letter">
+          청<span class="sm-letter">년</span>정<span class="sm-letter">책</span>지역
+        </div>
+      </div>
       <Link to={`/auth`}><div class='primary-btn'>숨은 청년정책 찾으러 가기</div></Link>
 
         
