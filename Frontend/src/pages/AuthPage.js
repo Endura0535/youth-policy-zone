1afe86a1919dab2bffbc5edc709899186fb9417e
pplemover <paulimbest@naver.com>
2023-09-10 23:56:05 +0900

    feat: 로그인/회원가입 탭 전환

--- Frontend/src/pages/AuthPage.js
+++ Frontend/src/pages/AuthPage.js
@@ -2,31 +2,39 @@ import React, { useState } from 'react'
 import Signin from './component/Signin';
 import Signup from './component/Signup';
 import './AuthPage.css'
+import shinhanlogo from '../assets/images/shinhanlogo.png';
 
 function AuthPage() {
 
   const [tab, setTabs] = useState(0);
+  const handleToggle = () => {
+    setTabs(prevTab => (prevTab === 0 ? 1 : 0));
+  };
 
 
   return (
-    <div>
-      <div>신한은행</div>
-      <div>청년정책지역</div>
+    <div class='center'>
+      <div class='brand yflex'>
+        <img src={shinhanlogo} alt="내 이미지" class="welcomelogo"/>
+        <div class="brand-letter">
+          청<span class="sm-letter">년</span>정<span class="sm-letter">책</span>지역
+        </div>
+      </div>
       <div>이곳은 청년 정책 지역, 청정지역입니다.</div>
 
       <div class="auth-toggle-container">
-	
           <label class="switch btn-color-mode-switch">
-                 <input value="1" id="color_mode" name="color_mode" type="checkbox" />
+              <input
+                value={tab}
+                id="color_mode"
+                name="color_mode"
+                type="checkbox"
+                onChange={handleToggle}
+              />
               <label class="btn-color-mode-switch-inner" data-off="로그인" data-on="회원가입" for="color_mode"></label>
           </label>
       </div>
 
-
-      <div>
-        <button onClick={() => setTabs(0)}>로그인</button>
-        <button onClick={() => setTabs(1)}>회원가입</button>
-      </div>
       {tab === 0 && <Signin />}
       {tab === 1 && <Signup />}
     </div>
