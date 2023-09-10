9cdb8358124e1378741deab79fa2c85f306a8e33
Donggyeom <eastcrtsy@gmail.com>
2023-09-10 13:56:52 +0900

    feat: 로그인 페이지 비밀번호 보기 기능

--- Frontend/src/pages/component/Signin.js
+++ Frontend/src/pages/component/Signin.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Signin() {
+
+  const [email, setEmail] = useState('');
+  const [pw, setPw] = useState('');
+  const [pwType, setPwType] = useState({
+    type: 'password',
+    visible: false,
+  });
+
+
+  const onEmailChanged = (e) => {
+    setEmail(e.target.value);
+  }
+
+  const onPwChanged = (e) => {
+    setPw(e.target.value);
+  }
+
+  const handlePwType = (e) => {
+    setPwType(() => {
+      if (!pwType.visible) {
+        return {type: "text", visible: true};
+      }
+      else {
+        return {type: "password", visible: false};
+      }
+    })
+  }
+
   return (
-    <div>Signin</div>
+    <div>
+        <div><label htmlFor="id">Email : <input type="email" onChange={onEmailChanged} value={email} id='id'/></label></div>
+        <div>
+          <label htmlFor="pw">Password : <input type={pwType.type} onChange={onPwChanged} value={pw} id='pw'/></label>
+          <span onClick={handlePwType}>
+            {pwType.visible ? "비밀번호 숨기기" : "비밀번호 보기"}
+          </span>
+        </div>
+    </div>
   )
 }
 
