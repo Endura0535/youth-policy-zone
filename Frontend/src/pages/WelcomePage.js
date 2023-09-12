639c43ffb0075e04d6855274ea5f75ebeaa31be3
pplemover <paulimbest@naver.com>
2023-09-12 10:01:26 +0900

    feat: welcome ui 완성

--- Frontend/src/pages/WelcomePage.js
+++ Frontend/src/pages/WelcomePage.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import shinhanlogo from '../assets/images/shinhanlogo.png';
+import youthimage from '../assets/images/youth.png';
 
 function WelcomePage() {
   return (
-    <div>
-      <div class='brand yflex'>
-        <img src={shinhanlogo} alt="내 이미지" class="welcomelogo"/>
-        <div class="brand-letter">
-          청<span class="sm-letter">년</span>정<span class="sm-letter">책</span>지역
-        </div>
+    <div class="welcome-container">
+      <div class='brand'>
+          <img src={shinhanlogo} alt="신한로고" class="logoimage"/>
+          <div class="brand-letter">
+            청<span class="sm-letter">년</span>정<span class="sm-letter">책</span>지역
+          </div>
       </div>
-      <Link to={`/auth`}><div class='primary-btn'>숨은 청년정책 찾으러 가기</div></Link>
+      <div class='cover-image'>
+          <img src={youthimage} alt="청년" class="youthimage"/>
+      </div>
+      <Link to={`/auth`} style={{ textDecoration: 'none' }} class='cover-btn'>
+        <div className='primary-btn'>숨은 청년정책 찾으러 가기</div>
+      </Link>
 
         
     </div>
