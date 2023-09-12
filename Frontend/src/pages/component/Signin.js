3eb0d88ce23933810cc11e9ac42eccd55b1be497
pplemover <paulimbest@naver.com>
2023-09-12 21:33:07 +0900

    feat: 로그인 UI

--- Frontend/src/pages/component/Signin.js
+++ Frontend/src/pages/component/Signin.js
@@ -61,19 +61,31 @@ function Signin() {
 
   return (
     <div>
-        <div>
+        {/* 이메일 입력 */}
+        <div class="input-container">
           <div><label htmlFor="email">이메일 주소</label></div>
-          <div><input type="email" onChange={onEmailChanged} value={email} id="email"/></div>
+          <input placeholder="Enter email" type="email" onChange={onEmailChanged} value={email} id="email"/>
+          <span>
+            <svg stroke="currentColor" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" stroke-width="2" stroke-linejoin="round" stroke-linecap="round"></path>
+            </svg>
+          </span>
         </div>
-        <div>
+
+        {/* 비밀번호 입력 */}
+        <div class="input-container">
           <div><label htmlFor="pw">비밀번호</label></div>
-          <div>
-            <input type={pwVisibility.type} onChange={onPwChanged} value={pw} id="pw"/>
-            <span onClick={handlePwVisibility}>
-              {pwVisibility.visible ? "비밀번호 숨기기" : "비밀번호 보기"}
-            </span>
-          </div>
+          <input placeholder="Enter password" type={pwVisibility.type} onChange={onPwChanged} value={pw} id="pw" />
+
+          <span onClick={handlePwVisibility}>
+            {/* <svg stroke="currentColor" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" stroke-width="2" stroke-linejoin="round" stroke-linecap="round"></path>
+              <path d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" stroke-width="2" stroke-linejoin="round" stroke-linecap="round"></path>
+            </svg> */}
+            {pwVisibility.visible ? "비밀번호 숨기기" : "비밀번호 보기"}
+          </span>
         </div>
+
         <div>
           <label htmlFor='rememberId'>
             <input type="checkbox" onChange={handleRememberId} value={rememberId} id="rememberId"/>
@@ -82,7 +94,9 @@ function Signin() {
           <br />
           <span onClick={onClickForgotPw}>비밀번호를 잊어버렸나요?</span>
         </div>
-        <button onClick={onClickSignin}>로그인</button>
+
+        {/* 로그인 버튼 */}
+        <button onClick={onClickSignin} class='primary-btn'>로그인</button>
     </div>
   )
 }
