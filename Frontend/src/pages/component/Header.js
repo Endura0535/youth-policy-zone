e8de85b7ff45dfa49a4d7ebc53c13f93ee92bccb
Kim Donggyeom <eastcrtsy@gmail.com>
2023-09-16 14:00:59 +0900

    feat: header 프레임

--- Frontend/src/pages/component/Header.js
+++ Frontend/src/pages/component/Header.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import { useMember } from '../../MemberContext'
 
 function Header() {
+  const { memberInfo } = useMember();
+  
   return (
     <div>
-      <p></p>
-      <div>Title</div>
+      <div>어서오세요, {memberInfo.current.name}님</div>
+      <div>주소</div>
+      <div>알림</div>
       <p></p>
     </div>
-  )
+  );
 }
 
 export default Header
