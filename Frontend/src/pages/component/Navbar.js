3390324700df829ac7a21f78ab5145ba1532278d
Kim Donggyeom <eastcrtsy@gmail.com>
2023-09-16 16:45:08 +0900

    fix: navbar idx 수정

--- Frontend/src/pages/component/Navbar.js
+++ Frontend/src/pages/component/Navbar.js
@@ -3,24 +3,24 @@ import { useHome } from '../../HomeContext';
 
 
 function Navbar() {
-  const { setTabIdx } = useHome();
+  const { setNavIdx } = useHome();
 
   return (
     <div className="home-navbar-container">
       <div className='navbar-button'>
-        <button onClick={() => {setTabIdx(0)}}>추천정책</button>
+        <button onClick={() => {setNavIdx(0)}}>추천정책</button>
       </div>
 
       <div className='navbar-button'>
-        <button onClick={() => {setTabIdx(1)}}>전체정책</button>
+        <button onClick={() => {setNavIdx(1)}}>전체정책</button>
       </div>
 
       <div className='navbar-button'>
-        <button onClick={() => {setTabIdx(3)}}>찜한정책</button>
+        <button onClick={() => {setNavIdx(2)}}>찜한정책</button>
       </div>
 
       <div className='navbar-button'>
-        <button onClick={() => {setTabIdx(4)}}>내 정보</button>
+        <button onClick={() => {setNavIdx(3)}}>내 정보</button>
       </div>
 
     </div>
