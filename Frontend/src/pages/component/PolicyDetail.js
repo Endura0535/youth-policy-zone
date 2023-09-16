ea6b755048baf480773e04847e6b0ec887945c19
pplemover <paulimbest@naver.com>
2023-09-17 04:14:11 +0900

    feat: alarm

--- Frontend/src/pages/component/PolicyDetail.js
+++ Frontend/src/pages/component/PolicyDetail.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import { useLocation, useParams } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import Header from './Header'
-import Navbar from './Navbar'
 
 import { CSSTransition } from 'react-transition-group';
 
@@ -9,6 +8,11 @@ function PolicyDetail() {
   const params = useParams();
   const location = useLocation();
   const policy = location.state.policy;
+  const navigate = useNavigate();
+
+  const onClickBackButton = () => {
+    navigate(-1);
+  }
 
   const id = params.id;
   console.log(policy);
@@ -24,35 +28,26 @@ function PolicyDetail() {
           <div className='section-header shinhan-color mg-top-sm'>{policy.name}</div>
           <div className='mg-top-sm'>
               <div class="scroll-snap-card">
-                <div class="slide policy-summary">
+                <div class="slide policy-qualify">
                     <p class="tip">한눈에 보는 정책 요약</p>
+                </div>
 
+                <div class="slide policy-summary">
+                    <p class="tip">체크리스트</p>
                     <div id="checklist">
                       <input id="01" type="checkbox" name="r" value="1" />
-                      <label for="01">학력: 제한없음</label>
+                      <label for="01">학력: {policy.academicAbility}</label>
                       <input id="02" type="checkbox" name="r" value="2" />
-                      <label for="02">Cheese</label>
+                      <label for="02">마감기한: {policy.applicationEnd}</label>
                       <input id="03" type="checkbox" name="r" value="3" />
                       <label for="03">Coffee</label>
+                      <input id="04" type="checkbox" name="r" value="4" />
+                      <label for="04">전공제한: {policy.major}</label>
                     </div>
                 </div>
 
-
-
-
-
-
-
-
-
-
-                <div class="slide policy-qualify">
-                    <p class="tip">신청자격</p>
-                </div>
-                <div class="slide policy-apply">
-                    <p class="tip">신청 방법</p>
-                </div>
             </div>
+            <button class="primary-btn policydetail-btn" onClick={onClickBackButton}>뒤로 가기</button>
           </div>
         </div>
       </CSSTransition>
